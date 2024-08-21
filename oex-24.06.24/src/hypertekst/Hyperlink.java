71d5ec2602c977b82d63e109b0f6748556b408fa
maximquantum <maximquantum148@gmail.com>
2024-08-21 19:05:09 +0200

    added field "doelelement"

--- oex-24.06.24/src/hypertekst/Hyperlink.java
+++ oex-24.06.24/src/hypertekst/Hyperlink.java
@@ -2,6 +2,12 @@ package hypertekst;
 
 public class Hyperlink extends Element {
 	
+	
+	/**
+	 * @peerObject
+	 */
+	Element doelelement;
+	
 	public Hyperlink(String tekstinhoud) {
 		super(tekstinhoud);
 	}
